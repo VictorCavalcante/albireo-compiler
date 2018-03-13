e8a30bbe1674a909a8efc9f330944e8481d183fc
Victor Cavalcante <victor.ra.cavalcante@gmail.com>
2018-03-13 16:55:11 -0300

    Adding HelloWorld.alb to resources, opening file with relative path;

--- src/main/java/br/ic/ufal/compiler/Main.java
+++ src/main/java/br/ic/ufal/compiler/Main.java
@@ -1,13 +1,21 @@
 package br.ic.ufal.compiler;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import br.ic.ufal.compiler.scanner.Scanner;
 
 /**
  * @author Victor Cavalcante
  */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello");
+
+    public static void main(String[] args) throws FileNotFoundException {
+        File albFile = new File(Main.getAbsolutePath("src/main/resources/HelloWorld.alb"));
         Scanner scanner = new Scanner();
     }
+
+    private static String getAbsolutePath(String filename) {
+        return System.getProperty("user.dir") + File.separator + filename;
+    }
+
 }
