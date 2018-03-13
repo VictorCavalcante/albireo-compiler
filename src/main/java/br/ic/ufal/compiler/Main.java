9f16e3b7ffb946c21ad61574fa0bbe07d6d0a18f
Victor Cavalcante <victor.ra.cavalcante@gmail.com>
2018-03-13 16:56:28 -0300

    Initializing scanner class; Reading buffer from file;

--- src/main/java/br/ic/ufal/compiler/Main.java
+++ src/main/java/br/ic/ufal/compiler/Main.java
@@ -1,17 +1,27 @@
 package br.ic.ufal.compiler;
 
+import br.ic.ufal.compiler.scanner.Scanner;
+import br.ic.ufal.compiler.token.InvalidTokenException;
+
 import java.io.File;
 import java.io.FileNotFoundException;
-import br.ic.ufal.compiler.scanner.Scanner;
+import java.io.IOException;
 
 /**
  * @author Victor Cavalcante
  */
 public class Main {
 
-    public static void main(String[] args) throws FileNotFoundException {
-        File albFile = new File(Main.getAbsolutePath("src/main/resources/HelloWorld.alb"));
-        Scanner scanner = new Scanner();
+    public static void main(String[] args) throws FileNotFoundException, InvalidTokenException {
+
+        try {
+            File albFile = new File(Main.getAbsolutePath("src/main/resources/HelloWorld.alb"));
+            Scanner scanner = new Scanner(albFile);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     private static String getAbsolutePath(String filename) {
--- src/main/java/br/ic/ufal/compiler/scanner/Scanner.java
+++ src/main/java/br/ic/ufal/compiler/scanner/Scanner.java
@@ -1,12 +1,35 @@
 package br.ic.ufal.compiler.scanner;
 
+import java.io.*;
+import br.ic.ufal.compiler.token.Token;
+
 public class Scanner {
 
-    public Scanner() {
-        //
+    private BufferedReader bufferReader;
+    private char currentChar;
+    private int row = 1;
+    private int col = 1;
+
+    public Scanner(File fileToParse) throws FileNotFoundException{
+        bufferReader = new BufferedReader(new FileReader(fileToParse));
+        currentChar = getNextChar();
+        System.out.println(currentChar);
     }
 
-    private void nextToken() {
-        //
+    public Token nextToken() {
+        return null;
     }
+
+    private char getNextChar(){
+        char nextChar;
+        try {
+            nextChar = (char) bufferReader.read();
+            col++;
+            return nextChar;
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
 }
